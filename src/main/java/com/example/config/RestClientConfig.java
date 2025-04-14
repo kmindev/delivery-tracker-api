981e8eb6863109676a098c2d01964b2254db588a
kmindev <kangkm0324@gmail.com>
2025-04-14 22:18:24 +0900

    feat: RestClient 설정

---
+++ src/main/java/com/example/config/RestClientConfig.java
@@ -0,0 +1,42 @@
+package com.example.config;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@RequiredArgsConstructor
+@Configuration
+public class RestClientConfig {
+
+    @Value("${delivery-tracker.client-id}")
+    private String clientId;
+    @Value("${delivery-tracker.client-secret}")
+    private String clientSecret;
+
+    @Bean
+    public RestClient restClient() {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
+        JdkClientHttpRequestFactory requestFactory = new JdkClientHttpRequestFactory(httpClient);
+        requestFactory.setReadTimeout(Duration.ofSeconds(5));
+        return RestClient.builder()
+                .baseUrl("https://apis.tracker.delivery/graphql")
+                .requestFactory(requestFactory)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, getAuthHeader())
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+    private String getAuthHeader() {
+        return "TRACKQL-API-KEY " + clientId + ":" + clientSecret;
+    }
+
+}
